eaddd8ac1f618bc2d083a78d1d2e4b808102f59d
C@rlosM <126515626+cmoreyra09@users.noreply.github.com>
2023-04-10 10:15:31 -0500

    Delete FUNCION  MAX.sql

--- FUNCION  MAX.sql
+++
@@ -1,9 +0,0 @@
-FUNCION  MAX
-
--- MAX (Columna)
---Cuando fue la ultima fecha y la primera fecha de la orden?
-SELECT 
-MAX(OrderDate) [LasDate],
-MIN(OrderDate) [FirstDate]
-FROM Sales.SalesOrderHeader
---Codigo que te permite tener la ultima fecha del registro de la orden .Asi mismo la primera orden de todos los registro--
